ce5fbb831a9839f25ca333027970874b79e85ca9
sfsf700 <hirokazu.2rnk@gmail.com>
2022-07-12 16:30:23 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   #end
 
   def create
-    post = Post.create(content: params[:content])
-    render json:{ post: post }
+    post = Post.create(content: params[:content]) #新たに投稿されたメモの内容を変数postに格納
+    render json:{ post: post } #{ post: post }というデータをJSON形式で返却することが出来る
   end
 end
